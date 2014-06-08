780047598c828731a32b73c16047e9ccf43130a5
jackjack-jj <jackjack-jj@users.noreply.github.com>
2014-06-08 19:45:02 +0200

    btpp 0.2.88

--- btpp.meta.js
+++ btpp.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          BitcoinTalk++
-// @version       0.2.87
+// @version       0.2.88
 // @author        jackjack-jj
 // @description   Adds lot of features to bitcointalk.org, including a vote system
 // @namespace     https://github.com/jackjack-jj
