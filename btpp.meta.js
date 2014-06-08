c3bed3b66343a956636b6305f052ff22d09c1cad
jackjack-jj <jackjack-jj@users.noreply.github.com>
2014-06-08 19:21:50 +0200

    create Meta file

---
+++ btpp.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name          BitcoinTalk++
+// @version       0.2.87
+// @author        jackjack-jj
+// @description   Adds lot of features to bitcointalk.org, including a vote system
+// @namespace     https://github.com/jackjack-jj
+// @homepageURL   http://btpp.jampa.eu/
+// @downloadURL   https://github.com/jackjack-jj/btpp/raw/master/btpp.user.js
+// @updateURL     https://github.com/jackjack-jj/btpp/raw/master/btpp.meta.js
+// @include       https://bitcointalk.org/*
+// @include       https://discussbitcoin.org/*
+// @include       http://jampa.eu/*
+// @include       https://jampa.eu/*
+// @include       http://btpp.jampa.eu/*
+// @include       https://btpp.jampa.eu/*
+// ==/UserScript==
